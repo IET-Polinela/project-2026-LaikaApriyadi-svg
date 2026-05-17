023f2fe2fb60796e7e462f4d73b97de0ae472042
laika <laikapriyadi4@gmail.com>
2026-05-17 14:13:19 +0700

    lab session 9_2

--- Lab_24782079/Laikaapriyadi_24782079_iet_2026/main_app/api_views.py
+++ Lab_24782079/Laikaapriyadi_24782079_iet_2026/main_app/api_views.py
@@ -6,3 +6,10 @@ class ReportViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny] # Akses publik untuk sementara
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+    
+def get_queryset(self):
+    user = self.request.user
+    if user.is_staff:
+        return Report.objects.all() # Admin lihat semua
+    # Citizen cuma lihat laporan yang sudah REPORTED atau DRAFT milik sendiri
+    return Report.objects.filter(Q(status='REPORTED') | Q(reporter=user)) 
