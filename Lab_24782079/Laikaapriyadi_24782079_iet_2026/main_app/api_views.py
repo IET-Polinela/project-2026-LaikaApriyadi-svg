cfdbe5cf9f0827972e99f8a018966f5bf14bc505
laika <laikapriyadi4@gmail.com>
2026-05-20 15:33:07 +0700

    lab session9_2

--- Lab_24782079/Laikaapriyadi_24782079_iet_2026/main_app/api_views.py
+++ Lab_24782079/Laikaapriyadi_24782079_iet_2026/main_app/api_views.py
@@ -1,15 +1,35 @@
 from rest_framework import viewsets, permissions
 from .models import Report
 from .serializers import ReportSerializer
+from .permissions import IsOwnerAndDraftOrReadOnly
+from django.db.models import Q
 
 class ReportViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.AllowAny] # Akses publik untuk sementara
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
-    
-def get_queryset(self):
-    user = self.request.user
-    if user.is_staff:
-        return Report.objects.all() # Admin lihat semua
-    # Citizen cuma lihat laporan yang sudah REPORTED atau DRAFT milik sendiri
-    return Report.objects.filter(Q(status='REPORTED') | Q(reporter=user)) 
+
+    def get_permissions(self):
+        """
+        Menentukan izin berdasarkan aksi yang dilakukan. 
+        """
+        # Jika user ingin edit (update) atau hapus (destroy) [cite: 58]
+        if self.action in ['update', 'partial_update', 'destroy']:
+            # Wajib login DAN lolos pengecekan Satpam Draft [cite: 59]
+            return [permissions.IsAuthenticated(), IsOwnerAndDraftOrReadOnly()]
+        
+        # Untuk aksi lainnya (List, Detail, Create), cukup login saja [cite: 60]
+        return [permissions.IsAuthenticated()]
+
+    def perform_create(self, serializer):
+        """
+        Mengisi field 'reporter' secara otomatis dari user yang sedang login. [cite: 51, 61, 62]
+        """
+        # Ini mencegah user 'menembak' nama reporter lain via JSON [cite: 62, 75]
+        serializer.save(reporter=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Report.objects.all() # Admin lihat semua
+        # Citizen cuma lihat laporan yang sudah REPORTED atau DRAFT milik sendiri
+        return Report.objects.filter(Q(status='REPORTED') | Q(reporter=user)) 
